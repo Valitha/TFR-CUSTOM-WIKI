0f89b0aa3bb20d2d259fa2a8ff3574a77943a856
Valitha <Valentina.Schroeder2006@gmail.com>
2026-08-21 03:58:29 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tfr-wiki-shell-v36';
+const CACHE_NAME = 'tfr-wiki-shell-v37';
 const HTML2CANVAS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js';
 const CORE = [
   HTML2CANVAS_URL,
