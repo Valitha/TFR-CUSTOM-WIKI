3bbda23ac8057f5b65e51e35f54340fb829354b1
Valitha <Valentina.Schroeder2006@gmail.com>
2026-08-21 11:13:43 +0100

    1.0.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tfr-wiki-shell-1.0';
+const CACHE_NAME = 'tfr-wiki-shell-1.0.1';
 const HTML2CANVAS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js';
 const CORE = [
   HTML2CANVAS_URL,
